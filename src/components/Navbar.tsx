02279d1e72c161a306f6ac706a2a90b30906d2d0
schatrath100 <schatrath100@gmail.com>
2025-05-28 23:52:28 -0700

    Add useEffect import to Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Bell, ChevronDown, Menu, User, LogOut } from 'lucide-react';
 import { Logo } from './Logo';
 import { useNavigate } from 'react-router-dom';
