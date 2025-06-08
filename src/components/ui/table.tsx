fc6b847f3006d94fd287bf39adac82480c79b48e
S Chatrath <schatrath@SChatrath-Laptop.local>
2025-06-08 13:50:31 -0700

    feat: make table headers bold for better visibility

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -72,7 +72,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "p-4 text-left align-middle font-medium text-gray-500 dark:text-gray-400 [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
+      "p-4 text-left align-middle font-bold text-gray-500 dark:text-gray-400 [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px]",
       className
     )}
     {...props}
